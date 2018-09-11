554fc9f8d364b51bea0886c72405f992c56c42a6
vikram yadav <vyadav@pivotal.io>
2018-09-11 15:36:33 -0400

    install go

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-apt-get update && \
+install_go() {
+	local go_url=https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+	local tmpdir=$(mktemp -d)
+
+	mkdir -p $tmpdir
+	pushd $tmpdir
+		wget $go_url
+		sudo tar -C /usr/local -xzf go*.tar.gz
+		echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
+	popd
+	rm -r $tmpdir
+}
+
+apt-get update
+apt-get install -y build-essential
 apt-get install -y libseccomp-dev libcap-dev
+
+install_go
