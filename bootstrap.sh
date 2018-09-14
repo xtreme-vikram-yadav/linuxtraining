709d905e7974cbfcffdf2fa7d1ac5ed4622d8858
bot <witchcraft@example.com>
2018-09-14 17:01:12 -0400

    Install files and quick fixes to redirect syntax

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,18 @@ install_go() {
 	local tmpdir=$(mktemp -d)
 
 	mkdir -p $tmpdir
-	pushd $tmpdir > /dev/null 2&>1
+	pushd $tmpdir > /dev/null 2>&1
 		wget $go_url > /dev/null 2>&1
 		sudo tar -C /usr/local -xzf go*.tar.gz
 		echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
-	popd > /dev/null 2&>1
+	popd > /dev/null 2>&1
 	rm -r $tmpdir
 }
 
 sudo apt-get update
 sudo apt-get install -y build-essential
-sudo apt-get install -y libseccomp-dev libcap-dev
+sudo apt-get install -y libseccomp-dev libcap-dev libacl1-dev libselinux-dev
+cd ~
+wget http://man7.org/lsp/course-files-2018-09-17-pivotal.tar.gz > /dev/null 2>&1
 
 install_go
